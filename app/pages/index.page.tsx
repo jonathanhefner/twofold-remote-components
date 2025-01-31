c1ec8b5265525b9b9dd5dee9e4583bc4f52801f8
Jonathan Hefner <jonathan@hefner.pro>
2025-01-31 15:28:40 -0600

    Copy demos from next-remote-components
    However, the "Passing Children" demo is temporarily disabled due to the
    following error:
    
        React Element cannot be passed to Server Functions from the Client without a temporary reference set. Pass a TemporaryReferenceSet to the options. {children: </>} ^^^
    
        resolveToJSON@http://localhost:3000/_assets/client-app/chunks/chunk-EP2G6W75.js:327:23
        serializeModel@http://localhost:3000/_assets/client-app/chunks/chunk-EP2G6W75.js:497:23
        processReply@http://localhost:3000/_assets/client-app/chunks/chunk-EP2G6W75.js:499:151
        node_modules/.pnpm/react-server-dom-webpack@19.0.0_react-dom@19.0.0_react@19.0.0__react@19.0.0_webpack@5.97.1/node_modules/react-server-dom-webpack/cjs/react-server-dom-webpack-client.browser.development.js/exports.encodeReply/<@http://localhost:3000/_assets/client-app/chunks/chunk-EP2G6W75.js:1827:35
        node_modules/.pnpm/react-server-dom-webpack@19.0.0_react-dom@19.0.0_react@19.0.0__react@19.0.0_webpack@5.97.1/node_modules/react-server-dom-webpack/cjs/react-server-dom-webpack-client.browser.development.js/exports.encodeReply@http://localhost:3000/_assets/client-app/chunks/chunk-EP2G6W75.js:1826:16
        callServer@http://localhost:3000/_assets/client-app/chunks/chunk-EP2G6W75.js:2208:50
        action@http://localhost:3000/_assets/client-app/chunks/chunk-EP2G6W75.js:1801:18
        <anonymous>@http://localhost:3000/_assets/client-app/entries/PassingChildren-LN7365XS.js:22:80
        getPromise/cacheEntry.promise<@http://localhost:3000/_assets/client-app/chunks/chunk-6IEOHCIU.js:66:77

--- app/pages/index.page.tsx
+++ app/pages/index.page.tsx
@@ -1,19 +1,56 @@
-export default async function Page() {
-  return (
-    <>
-      <title>Welcome to Twofold</title>
-      <div>
-        <h1 className="text-4xl font-extrabold tracking-tighter">
-          Welcome to Twofold
-        </h1>
-        <p className="mt-4 max-w-prose text-lg">
-          Create your first React Server Component by editing{" "}
-          <span className="rounded bg-gray-100 px-1 py-0.5 font-mono text-sm">
-            app/pages/index.page.tsx
-          </span>
-          .
-        </p>
-      </div>
-    </>
-  );
+import { DynamicSelect } from "../components/DynamicSelect"
+import { EventCalendar } from "../components/EventCalendar"
+import { PassingChildren } from "../components/PassingChildren"
+import { Typeahead } from "../components/Typeahead"
+import { Waterfall } from "../components/Waterfall"
+import "./page.css"
+
+export default function Page() {
+  return <>
+    <section className="typeahead">
+      <h2>Typeahead</h2>
+      <p>
+        <a target="_blank" href="https://github.com/jonathanhefner/twofold-remote-components/blob/main/app/components/Typeahead.tsx">client <code>.tsx</code></a>{" | "}
+        <a target="_blank" href="https://github.com/jonathanhefner/twofold-remote-components/blob/main/app/components/Typeahead.server.tsx">server <code>.tsx</code></a>
+      </p>
+      <div className="demo"><Typeahead /></div>
+    </section>
+
+    <section className="dynamic-select">
+      <h2>Dynamic Select</h2>
+      <p>
+        <a target="_blank" href="https://github.com/jonathanhefner/twofold-remote-components/blob/main/app/components/DynamicSelect.tsx">client <code>.tsx</code></a>{" | "}
+        <a target="_blank" href="https://github.com/jonathanhefner/twofold-remote-components/blob/main/app/components/DynamicSelect.server.tsx">server <code>.tsx</code></a>
+      </p>
+      <div className="demo"><DynamicSelect /></div>
+    </section>
+
+    <section className="event-calendar">
+      <h2>Event Calendar</h2>
+      <p>
+        <a target="_blank" href="https://github.com/jonathanhefner/twofold-remote-components/blob/main/app/components/EventCalendar.tsx">client <code>.tsx</code></a>{" | "}
+        <a target="_blank" href="https://github.com/jonathanhefner/twofold-remote-components/blob/main/app/components/EventCalendar.server.tsx">server <code>.tsx</code></a>
+      </p>
+      <div className="demo"><EventCalendar /></div>
+    </section>
+
+    <section className="passing-children">
+      <h2>Passing Children</h2>
+      <p>
+        <a target="_blank" href="https://github.com/jonathanhefner/twofold-remote-components/blob/main/app/components/PassingChildren.tsx">client <code>.tsx</code></a>{" | "}
+        <a target="_blank" href="https://github.com/jonathanhefner/twofold-remote-components/blob/main/app/components/PassingChildren.server.tsx">server <code>.tsx</code></a>
+      </p>
+      {/* <div className="demo"><PassingChildren /></div> */}
+      <div className="demo"><strong><em>TEMPORARILY DISABLED</em></strong></div>
+    </section>
+
+    <section className="waterfall">
+      <h2>Waterfall</h2>
+      <p>
+        <a target="_blank" href="https://github.com/jonathanhefner/twofold-remote-components/blob/main/app/components/Waterfall.tsx">client <code>.tsx</code></a>{" | "}
+        <a target="_blank" href="https://github.com/jonathanhefner/twofold-remote-components/blob/main/app/components/Waterfall.server.tsx">server <code>.tsx</code></a>
+      </p>
+      <div className="demo"><Waterfall /></div>
+    </section>
+  </>
 }
